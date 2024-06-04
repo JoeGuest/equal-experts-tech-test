f148cb994452f18f21e4c1b605054019ce5c1454
Joe Guest <joe.a.guest@gmail.com>
2024-06-04 14:06:58 +0100

    shopping list with items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,35 @@
+const shoppingList = [
+  "Apples",
+  "Bananas",
+  "Carrots",
+  "Cherries",
+  "Dates",
+  "Eggs",
+  "Grapes",
+  "Honey",
+  "Lemons",
+  "Mangoes",
+  "Oranges",
+  "Peacheses",
+  "Pears",
+  "Pineapples",
+  "Plums",
+  "Strawberries",
+  "Watermelon",
+];
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-equal-experts-blue p-24">
+    <main className="flex min-h-screen flex-col items-center gap-4 bg-equal-experts-blue p-24">
       <h1 className="font-primary text-4xl font-bold text-white">
         Equal Experts Shopping List
       </h1>
 
-      <h2 className="font-secondary text-4xl font-bold text-white">
-        Test second font
-      </h2>
+      {shoppingList.map((item) => (
+        <div key={item} className="font-secondary text-2xl text-white">
+          {item}
+        </div>
+      ))}
     </main>
   );
 }
