2600199b1851b091160f5558fcaf1d8eb7f94632
Joe Guest <joe.a.guest@gmail.com>
2024-06-04 14:08:53 +0100

    wrap list in container div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,16 +20,17 @@ const shoppingList = [
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center gap-4 bg-equal-experts-blue p-24">
+    <main className="flex min-h-screen flex-col items-center gap-8 bg-equal-experts-blue p-24">
       <h1 className="font-primary text-4xl font-bold text-white">
         Equal Experts Shopping List
       </h1>
-
-      {shoppingList.map((item) => (
-        <div key={item} className="font-secondary text-2xl text-white">
-          {item}
-        </div>
-      ))}
+      <div className="flex flex-col gap-4">
+        {shoppingList.map((item) => (
+          <div key={item} className="font-secondary text-2xl text-white">
+            {item}
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
